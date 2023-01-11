70ad3910176d9735039be539b5ec921c2e4af7d7
Victor <garciavictorm997@gmail.com>
2023-01-11 11:48:09 -0800

    ci: conversion de tipo de datos

--- js/js01/js01.js
+++ js/js01/js01.js
@@ -1,1 +1,18 @@
 console.log("sesion js01");
+/*7 datos primitivos
+-string("hola mundo")
+-number(1800)
+-bigInt(1800n) los numeros se almacenan 64 bits para almacenar
+y 63 para representarlos
+-boolean(false/true)
+-undefined(undefined) un dato no se define el tipo de dato
+-null(null) intencionalmente se borra el tio de dato
+-symbol
+
+tipos de datos bject= {clave:valor, datoboolean:true})
+-array
+const arra = ["valor", 0];
+funciones
+
+
+*/
